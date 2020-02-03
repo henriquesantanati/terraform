0a0bd09f8b2ee922c3a19881feed21e37ccf9074
Henrique <cssntan@amazon.com>
2020-02-03 14:28:15 +0000

    Subnets

---
+++ Network/subnet.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "public-subnet1" {
+  count             = 3
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet_cidr_[count.index]
+  availability_zone = data.aws_availability_zones.all.names[count.index]
+
+  tags {
+    Name = "Public Subnet [count.index]"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  count             = 3
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet_cidr_[count.index]
+  availability_zone =   availability_zone = "data.aws_availability_zones.all.names[count.index]"
+
+  tags {
+    Name = "Private Subnet [count.index]"
+  }
+}
+
+data "aws_availability_zones" "all" {}
