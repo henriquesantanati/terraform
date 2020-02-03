c46691e5466b498983707321ebb86239b7ce7042
Henrique <cssntan@amazon.com>
2020-02-03 17:40:47 +0000

    Private RT

--- Network/routetable.tf
+++ Network/routetable.tf
@@ -7,7 +7,19 @@ resource "aws_route_table" "public_route_table" {
   }
 
   tags = {
-    Name = "Public Subnet Route Table"
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    # default
+  }
+
+  tags = {
+    Name = "Private Route Table"
   }
 }
 
@@ -24,4 +36,19 @@ resource "aws_route_table_association" "route_table_link_2" {
 resource "aws_route_table_association" "route_table_link_3" {
   subnet_id      = aws_subnet.public-subnet3.id
   route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "route_table_link_1" {
+  subnet_id      = aws_subnet.private-subnet1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "route_table_link_2" {
+  subnet_id      = aws_subnet.private-subnet1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "route_table_link_3" {
+  subnet_id      = aws_subnet.private-subnet1.id
+  route_table_id = aws_route_table.private_route_table.id
 }
