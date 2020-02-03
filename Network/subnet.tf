bd893601cd2a24a3f9ad906c932e4a5d72a49e07
Henrique <cssntan@amazon.com>
2020-02-03 16:17:34 +0000

    Failed to recognize the value of this number literal.

--- Network/subnet.tf
+++ Network/subnet.tf
@@ -3,9 +3,9 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_subnet" "public-subnet1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = 10.0.1.0/24
-  availability_zone = sa-east-1a
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = sa-east-1a
   map_public_ip_on_launch = true
 
   tags = {
@@ -14,9 +14,9 @@ resource "aws_subnet" "public-subnet1" {
 }
 
 resource "aws_subnet" "public-subnet2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = 10.0.2.0/24
-  availability_zone = sa-east-1b
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = sa-east-1b
   map_public_ip_on_launch = true
 
   tags = {
@@ -25,9 +25,9 @@ resource "aws_subnet" "public-subnet2" {
 }
 
 resource "aws_subnet" "public-subnet3" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = 10.0.3.0/24
-  availability_zone = sa-east-1c
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = sa-east-1c
   map_public_ip_on_launch = true
 
   tags = {
@@ -37,7 +37,7 @@ resource "aws_subnet" "public-subnet3" {
 
 resource "aws_subnet" "private-subnet1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = 10.0.10.0/24
+  cidr_block        = "10.0.10.0/24"
   availability_zone = sa-east-1a
 
   tags = {
@@ -47,7 +47,7 @@ resource "aws_subnet" "private-subnet1" {
 
 resource "aws_subnet" "private-subnet2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = 10.0.20.0/24
+  cidr_block        = "10.0.20.0/24"
   availability_zone = sa-east-1b
 
   tags = {
@@ -57,7 +57,7 @@ resource "aws_subnet" "private-subnet2" {
 
 resource "aws_subnet" "private-subnet3" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = 10.0.30.0/24
+  cidr_block        = "10.0.30.0/24"
   availability_zone = sa-east-1c
 
   tags = {
