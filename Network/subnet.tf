b2838163691f85cea91ab9c20b2759566764c780
Henrique <cssntan@amazon.com>
2020-02-03 14:41:52 +0000

    you should use = to represent the fact that you are assigning a map value to that argument, rather than creating a nested object of type tags

--- Network/subnet.tf
+++ Network/subnet.tf
@@ -2,24 +2,24 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_subnet" "public-subnet1" {
+resource "aws_subnet" "public-subnet[count.index]" {
   count             = 3
   vpc_id            = aws_vpc.main.id
   cidr_block        = "var.public_subnet_cidr_[count.index]"
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
-  tags {
+  tags = {
     Name = "Public Subnet [count.index]"
   }
 }
 
-resource "aws_subnet" "private-subnet" {
+resource "aws_subnet" "private-subnet[count.index]" {
   count             = 3
   vpc_id            = aws_vpc.main.id
   cidr_block        = "var.private_subnet_cidr_[count.index]"
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
-  tags {
+  tags = {
     Name = "Private Subnet [count.index]"
   }
 }
