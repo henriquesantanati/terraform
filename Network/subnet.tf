564dd3bfe9af4e87a649936ab778ef430148994a
Henrique <cssntan@amazon.com>
2020-02-03 14:29:35 +0000

    An argument definition must end with a newline

--- Network/subnet.tf
+++ Network/subnet.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "private-subnet" {
   count             = 3
   vpc_id            = aws_vpc.default.id
   cidr_block        = var.public_subnet_cidr_[count.index]
-  availability_zone =   availability_zone = "data.aws_availability_zones.all.names[count.index]"
+  availability_zone = data.aws_availability_zones.all.names[count.index]
 
   tags {
     Name = "Private Subnet [count.index]"
